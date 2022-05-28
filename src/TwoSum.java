355588c70e5606f9ef1dbf93239a20a7a63b5efe
Evgeny <e.kolomiets@aol.com>
2022-05-28 08:42:51 -0700

    updated TwoSum

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,40 +1,67 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class TwoSum {
     public static void main (String args[]) {
-        int[] arr1 = new int[] {3, 5, 7, 2};
-        int [] arr2 = new int [] {5, 1, 2, 3, 7, 9};
-        int target = 4;
 
-        System.out.println(two_sum(arr1, arr2, target));
+        Scanner scanner = new Scanner(System.in);
+
+        int l;
+        int m;
+        int target;
+
+
+        System.out.println("Введите длину массива 1");
+        l = scanner.nextInt();
+        ArrayList<Integer> arr1 = new ArrayList<Integer>();
+        arr1.add(l);
+        System.out.println("Введите значения элементов массива");
+        for (int i = 0; i < l; i++){
+            arr1.add(scanner.nextInt());
+        }
+
+        System.out.println("Введите длину массива 2");
+        m = scanner.nextInt();
+        ArrayList<Integer> arr2 = new ArrayList<Integer>();
+        arr2.add(m);
+        System.out.println("Введите значения элементов массива");
+        for (int i = 0; i < m; i++){
+            arr2.add(scanner.nextInt());
+        }
+
+        System.out.println("Введите целевое число");
+        target = scanner.nextInt();
+
+        System.out.println("Количество совпадений: " + two_sum(arr1, arr2, target));
 
     }
 
-    public static int two_sum (int[] arr1, int[] arr2, Integer n){
+    public static int two_sum (ArrayList<Integer> arr1, ArrayList<Integer> arr2, Integer n){
         int result = 0;
 
         Map<Integer, Integer> map1 = new HashMap<>();
-        for (int i = 1; i < arr1.length; i++) {
-            map1.put(arr1[i], i);
+        for (int i = 1; i < arr1.size(); i++) {
+            map1.put(arr1.get(i), i);
         }
 
         Map<Integer, Integer> map2 = new HashMap<>();
-        for (int i = 1; i < arr2.length; i++){
-            map2.put(arr2[i], i);
+        for (int i = 1; i < arr2.size(); i++){
+            map2.put(arr2.get(i), i);
         }
 
-        if (arr1[0] >= arr2[0]){
-            for (int i = 1; i < arr1.length; i++){
-                int complement = n - arr1[i];
+        if (arr1.get(0) >= arr2.get(0)){
+            for (int i = 1; i < arr1.size(); i++){
+                int complement = n - arr1.get(i);
                 if (map2.containsKey(complement)){
                     result ++;
                 }
             }
         }
         else {
-            for (int i = 1; i < arr2.length; i++){
-                int complement = n - arr2[i];
+            for (int i = 1; i < arr2.size(); i++){
+                int complement = n - arr2.get(i);
                 if (map1.containsKey(complement)){
                     result ++;
                 }
